f463c42f94002f9ff0dfa983c8b1f325acf778da
nachh07 <ignaduplaa97@gmail.com>
2024-03-12 22:47:03 -0300

    Mocker corrected

--- tests.py
+++ tests.py
@@ -1,18 +1,20 @@
 # test_main.py
 import pytest
+from unittest.mock import MagicMock
 from app import fetch_pokemon_data, get_pokemon_names
 
 @pytest.fixture
-def mock_fetch_pokemon_data(requests_mock):
-    mock_response = {
+def requests_mock(mocker):
+    mock_response = MagicMock()
+    mock_response.json.return_value = {
         "results": [
             {"name": "bulbasaur"},
             {"name": "charmander"},
             {"name": "squirtle"}
         ]
     }
-    requests_mock.get("http://pokeapi.co/api/v2/pokemon/", json=mock_response)
-    return mock_response
+    return mocker.patch('main.requests.get', return_value=mock_response)
+
 
 def test_fetch_pokemon_data_success(mock_fetch_pokemon_data):
     data = fetch_pokemon_data()
