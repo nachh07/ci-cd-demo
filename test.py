136f1507358539cf552cce1d9490d483581e64b1
nachh07 <ignaduplaa97@gmail.com>
2024-03-12 23:02:39 -0300

    Tests corrected

--- test.py
+++ test.py
@@ -1,24 +1,31 @@
-# test_main.py
 import pytest
+from unittest.mock import MagicMock
 from app import fetch_pokemon_data, get_pokemon_names
 
 @pytest.fixture
-def mock_fetch_pokemon_data(requests_mock):
-    mock_response = {
+def requests_mock(mocker):
+    mock_response = MagicMock()
+    mock_response.json.return_value = {
         "results": [
             {"name": "bulbasaur"},
             {"name": "charmander"},
             {"name": "squirtle"}
         ]
     }
-    requests_mock.get("http://pokeapi.co/api/v2/pokemon/", json=mock_response)
-    return mock_response
+    return mocker.patch('main.requests.get', return_value=mock_response)
 
-def test_fetch_pokemon_data_success(mock_fetch_pokemon_data):
+def test_fetch_pokemon_data_success(requests_mock):
     data = fetch_pokemon_data()
-    assert data == mock_fetch_pokemon_data
+    assert data == {
+        "results": [
+            {"name": "bulbasaur"},
+            {"name": "charmander"},
+            {"name": "squirtle"}
+        ]
+    }
 
-def test_get_pokemon_names(mock_fetch_pokemon_data):
-    names = get_pokemon_names(mock_fetch_pokemon_data)
+def test_get_pokemon_names(requests_mock):
+    data = fetch_pokemon_data()
+    names = get_pokemon_names(data)
     expected_names = ["bulbasaur", "charmander", "squirtle"]
     assert names == expected_names
